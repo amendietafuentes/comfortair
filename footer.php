5633346d139e2393587e03e4d8274d23b2da0773
Arnulfo Mendieta <mendieta.arnulfo@gmail.com>
2022-09-26 01:49:22 -0500

    Added and register widgets columns for footer theme

--- footer.php
+++ footer.php
@@ -13,18 +13,48 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'comfort-air' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'comfort-air' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'comfort-air' ), 'comfort-air', '<a href="http://underscores.me/">Arnulfo Mendieta</a>' );
-				?>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 pt-5 pb-5">
+						<div class="row">
+							<h3 class="title-communities-col-one">Communities we serve</h3>
+							<div class="col-md-6">
+								<?php
+									dynamic_sidebar( 'footer-widget-communities-col-one' );
+								?>
+							</div>
+							<div class="col-md-6">
+								<?php
+									dynamic_sidebar( 'footer-widget-communities-col-two' );
+								?>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 pt-5 pb-5">
+						<?php
+							dynamic_sidebar( 'footer-widget-quick-menu-col-one' );
+						?>
+					</div>
+					<div class="col-md-4 pt-5 contact-footer-col-menu">
+						<?php
+							dynamic_sidebar( 'footer-widget-contact-menu-col-one' );
+						?>
+					</div>
+				</div>
+			</div>
 		</div><!-- .site-info -->
+		<div class="site-copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+
+					</div>
+					<div class="col-md-6">
+						<p class="copyright-disclaimer">&copy; <?php echo date("Y"); ?>. Comfort Air Heating and Cooling. All Rights Reserved.</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
